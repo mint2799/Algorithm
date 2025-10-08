b259d46bfa80a2006e5576a527e6c373698dcb36
성희 <shk9963@gmail.com>
2025-10-09 02:53:53 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    string a, b;
+    cin >> a >> b;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    cout << max(a, b) << "\n";
+}
