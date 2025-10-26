4c7f23f42e1c7fca4377fa6f180f775770d4906e
성희 <shk9963@gmail.com>
2025-10-27 04:59:34 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+using namespace std;
+
+int n, m;
+int arr[MAX];
+int res[MAX];
+bool visited[MAX];
+
+void dfs(int start, int depth);
+
+int main()
+{
+	cin >> n >> m; 
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	sort(arr, arr + n);
+	dfs(1,0);
+	return 0;
+}
+
+void dfs(int start, int depth) {
+	if (depth == m) {
+		for (int i = 0; i < m; i++)
+			cout << res[i] << ' ';
+		cout << "\n";
+	}else {
+		for (int i = start; i <= n; i++) {
+			if (!visited[i]) {
+				visited[i] = true;
+				res[depth] = arr[i - 1];
+				dfs(i + 1, depth + 1);
+				visited[i] = false;
+			}
+		}
+	}
+}
