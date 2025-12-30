68540cb2ce55a14ba073cd1a5bff1fb8770ec1ff
성희 <shk9963@gmail.com>
2025-12-31 04:24:25 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, cnt = 0;
+    cin >> n;
+    vector<int> A(n, 0);
+    
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+    sort(A.begin(), A.end());
+    
+    for (int k = 0; k < n; k++){
+        long find = A[k];
+        int i = 0, j = n - 1;
+        
+        while (i < j){
+            if (A[i] + A[j] == find){
+                if (i != k && j != k){
+                    cnt++;
+                    break;
+                }
+                else if (i == k)
+                    i++;
+                else if (j == k)
+                    j--;
+            }
+            else if (A[i] + A[j] < find)
+                i++;
+            else
+                j--;
+        }
+    }
+    cout << cnt << "\n";    
+}
