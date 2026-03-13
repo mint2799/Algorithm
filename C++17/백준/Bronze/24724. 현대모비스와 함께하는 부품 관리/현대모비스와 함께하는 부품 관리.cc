e74b94c9d353f21e6c78b2a6b751a26464068481
김성희 <shk9963@gmail.com>
2026-03-14 02:28:31 +0900

    [Bronze IV] Title: 현대모비스와 함께하는 부품 관리, Time: 104 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    for (int i=1; i<=t; i++){
+        int n;
+        long long a,b;
+        cin >> n >> a >> b;
+        for (int j=0; j<n; j++){
+            long long u,v;
+            cin >> u >> v;
+        }
+        cout << "Material Management " << i << "\n";
+        cout << "Classification ---- End!\n";    
+    }
+    return 0;
+}
