a7778572a9d1ef98e28bf5d08d9f7be7031b3fee
Seonghui Kim <shk9963@gmail.com>
2026-04-01 02:23:45 +0900

    [Bronze IV] Title: 선물, Time: 84 ms, Memory: 42660 KB -BaekjoonHub

---
+++ Python/백준/Bronze/28281. 선물/선물.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+min_sum = float('inf')
+
+for i in range(N - 1):
+    min_sum = min(min_sum, A[i] + A[i + 1])
+
+print(min_sum * X)
