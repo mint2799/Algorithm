90963c790c55a990bafbd16f2923531686cc6e8f
Seonghui Kim <shk9963@gmail.com>
2026-04-02 02:09:48 +0900

    [Unrated] Title: Rick, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Unrated/15639. Rick/Rick.py
@@ -0,0 +1,6 @@
+n = str(input())
+no = ['Give you up', 'Let you down', 'Run around and desert you', 'Make you cry', 'Say goodbye', 'Tell a lie and hurt you']
+if n in no:
+    print("NO")
+else:
+    print("YES")
